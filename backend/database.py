672c61c540fa3481f97e36e8c9edfc9f29fcab4f
Guhan Sattanathan <guhansattanathan@gmail.com>
2025-10-27 16:15:05 -0400

    Add multiple cursors for data collection

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,15 @@ import os
 db_path = os.path.join(os.path.dirname(__file__), '../data/nba.sqlite')
 
 db = sqlite3.connect(db_path)
-cursor = db.cursor()
+cursor1 = db.cursor()
+cursor2 = db.cursor()
 
-cursor.execute("SELECT * FROM game WHERE team_name_home = 'Toronto Huskies';")
-print(cursor.fetchall())
+cursor1.execute("SELECT * FROM game;")
+cursor2.execute("SELECT * FROM draft_combine_stats;")
+
+game_data = cursor1.fetchall()
+combine_data = cursor2.fetchall()
+
+print(game_data)
+print(combine_data)
+
